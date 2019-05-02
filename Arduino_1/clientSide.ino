eb77ab91fdf689c60aec7d2d9708d3f838ea4df4
Omid Ketabchi <ketabchi.omid@hotmail.com>
2019-05-02 11:32:33 -0700

    Add project files.

---
+++ Arduino_1/clientSide.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Arduino_1.ino
+ Created:	5/2/2019 10:40:34 AM
+ Author:	O.Ketabchi
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ serverSide/serverSide.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		serverSide.ino
+ Created:	5/2/2019 10:41:18 AM
+ Author:	O.Ketabchi
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
